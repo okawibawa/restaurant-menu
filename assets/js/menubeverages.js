74d8791a0b64c8b18d65dcae5d76c1f84f80009c
Oka Wibawa <oka@cakeplabs.com>
2020-07-22 00:18:41 +0800

    added the revision and finished the project

--- assets/js/menubeverages.js
+++ assets/js/menubeverages.js
@@ -3,12 +3,20 @@ const linkBevOne = document.getElementById('link-bev-one');
 const linkBevTwo = document.getElementById('link-bev-two');
 const linkBevThree = document.getElementById('link-bev-three');
 const linkBevFour = document.getElementById('link-bev-four');
+const linkBevFive = document.getElementById('link-bev-five');
+const linkBevSix = document.getElementById('link-bev-six');
+const linkBevSeven = document.getElementById('link-bev-seven');
+const linkBevEight = document.getElementById('link-bev-eight');
 
 // Menus
 const bevOne = document.getElementById('bevOne');
 const bevTwo = document.getElementById('bevTwo');
 const bevThree = document.getElementById('bevThree');
 const bevFour = document.getElementById('bevFour');
+const bevFive = document.getElementById('bevFive');
+const bevSix = document.getElementById('bevSix');
+const bevSeven = document.getElementById('bevSeven');
+const bevEight = document.getElementById('bevEight');
 
 // Functions
 linkBevOne.addEventListener('click', (e) => {
@@ -19,6 +27,10 @@ linkBevOne.addEventListener('click', (e) => {
   bevTwo.style.display = 'none';
   bevThree.style.display = 'none';
   bevFour.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
 });
 
 linkBevTwo.addEventListener('click', (e) => {
@@ -29,6 +41,10 @@ linkBevTwo.addEventListener('click', (e) => {
   bevOne.style.display = 'none';
   bevThree.style.display = 'none';
   bevFour.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
 });
 
 linkBevThree.addEventListener('click', (e) => {
@@ -39,6 +55,10 @@ linkBevThree.addEventListener('click', (e) => {
   bevOne.style.display = 'none';
   bevTwo.style.display = 'none';
   bevFour.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
 });
 
 linkBevFour.addEventListener('click', (e) => {
@@ -46,6 +66,66 @@ linkBevFour.addEventListener('click', (e) => {
 
   bevFour.style.display = 'block';
 
+  bevThree.style.display = 'none';
+  bevOne.style.display = 'none';
+  bevTwo.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
+});
+
+linkBevFive.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  bevFive.style.display = 'block';
+
+  bevFour.style.display = 'none';
+  bevThree.style.display = 'none';
+  bevOne.style.display = 'none';
+  bevTwo.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
+});
+
+linkBevSix.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  bevSix.style.display = 'block';
+
+  bevFive.style.display = 'none';
+  bevFour.style.display = 'none';
+  bevThree.style.display = 'none';
+  bevOne.style.display = 'none';
+  bevTwo.style.display = 'none';
+  bevSeven.style.display = 'none';
+  bevEight.style.display = 'none';
+});
+
+linkBevSeven.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  bevSeven.style.display = 'block';
+
+  bevSix.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevFour.style.display = 'none';
+  bevThree.style.display = 'none';
+  bevOne.style.display = 'none';
+  bevTwo.style.display = 'none';
+  bevEight.style.display = 'none';
+});
+
+linkBevEight.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  bevEight.style.display = 'block';
+
+  bevSeven.style.display = 'none';
+  bevSix.style.display = 'none';
+  bevFive.style.display = 'none';
+  bevFour.style.display = 'none';
   bevThree.style.display = 'none';
   bevOne.style.display = 'none';
   bevTwo.style.display = 'none';
